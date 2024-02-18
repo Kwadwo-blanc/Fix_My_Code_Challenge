9bdeaeed568362128726c3f555071c9f9c5388d0
Kwadwo-blanc <ebenezerghart@gmail.com>
2024-02-18 12:46:35 -0800

    edit

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -7,7 +7,8 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2)
+{
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
@@ -16,9 +17,11 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2], 10)
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+for (let i = 0 ; i < size ; i ++)
+{
+for (let j = 0 ; j < size ; j ++)
+{
+process.stdout.write("#");
+}
+process.stdout.write("\n");
 }
